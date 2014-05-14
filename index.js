522ac2032941ecbc7fc256f9c10d9121d87a2f85
djvanderlaan <djvanderlaan@unrealizedtime.nl>
2014-05-14 11:55:21 +0200

    Barebones version of starting jobs and reading status

--- index.js
+++ index.js
@@ -1,15 +1,43 @@
-// just bare bones for now (see http://mcagave.me/node-restify)
 var restify = require('restify');
 
-function respond(req, res, next) {
-  res.send('hello ' + req.params.name);
-  next();
+
+var jobs = [];
+
+function start_job(id) {
+  jobs[id].status = "running";
+  setTimeout(function() {
+    jobs[id].status = "finished";
+  }, 10000);
 }
 
+
+function new_job(req, res, next) {
+  if (req.is("application/json")) {
+    id = jobs.length;
+    jobs[id] = req.body;
+    jobs[id].ref = server.url + "/service/jobs/" + id;
+    jobs[id].status = "scheduled";
+    start_job(id);
+    res.status(201);
+    res.header("Location", jobs[id].ref);
+    res.send(jobs[id]);
+  } else {
+    next(new Error("Invalid data format."));
+  }
+}
+
+function get_job(req, res, next) {
+  res.send(jobs[+req.params.id]);
+}
+
+
 var server = restify.createServer();
-server.get('/hello/:name', respond);
-server.head('/hello/:name', respond);
+server.pre(restify.pre.userAgentConnection());
+server.use(restify.bodyParser());
+server.post('/service', new_job);
+server.get('/service/jobs/:id', get_job);
 
 server.listen(8080, function() {
   console.log('%s listening at %s', server.name, server.url);
 });
+
