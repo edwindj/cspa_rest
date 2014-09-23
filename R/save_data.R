f241fd5a874fd2b3bec47f3eb67a7448c8fe3475
Edwin de Jonge <edwindjonge@gmail.com>
2014-09-23 09:09:25 +0200

    added save_data.R

---
+++ R/save_data.R
@@ -0,0 +1,16 @@
+# needs jts functions!
+save_data_plus_schema <- function(x, path, improve_schema=function(s) s){
+  
+  cat("* Writing csv '", path, "'\n", sep="")
+  write.csv( x,
+             file=path,
+             row.names=FALSE,
+             na=""
+  )
+  
+  path_schema <- sub("(\\.csv)$", "_schema.json", path)
+  x_schema <- get_jts(x, improve_schema=improve_schema)
+  
+  cat("* Writing schema '", path_schema, "'\n", sep="")
+  writeLines(toJSON(x_schema, pretty = TRUE, auto_unbox = TRUE, force=TRUE), con=path_schema)
+}
